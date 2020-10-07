2ef9f30d5f5a7582540c0844c5a10c7791848dc1
Aditi Kajale <55836122+Aditi-kajale@users.noreply.github.com>
2020-10-07 09:59:52 +0530

    Add files via upload

---
+++ day2_a.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+  int *arr, sum, n , i, j, sumnew, flag;
+  printf("Enter no of elements :-");
+  scanf("%d", &n);
+  printf("Enter elements :-");
+  arr = (int *)malloc(sizeof(int)*n);
+  for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+  printf("Enter sum :-");
+  scanf("%d", &sum);
+  for(i = 0; i<n-1;i++)
+  {
+    flag =0; 
+    sumnew = arr[i];
+    for(j = i+1; j<n;j++)
+    {
+      sumnew += arr[j];
+      if(sumnew == sum)
+      {
+        flag =1;
+        break;
+      }
+    }
+    if(flag)
+      break;
+  }
+  if(flag)
+    printf("SUM found between indexes %d and %d", i, j);
+  else
+    printf("SUM not found");
+  return 0;
+}
---
+++ day2_b.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+  int i, j, flag=0, suml,sumr,n, *arr ;
+  printf("Enter no of elements :-");
+  scanf("%d", &n);
+  printf("Enter elements :-");
+  arr = (int *)malloc(sizeof(int)*n);
+  for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+  for(i=1;i<n-1;i++)
+  {
+    suml=0;
+    sumr=0;
+    for(j=i-1;j>=0;j--)
+      suml+=arr[j];
+    for(j=i+1;j<n;j++)
+      sumr+=arr[j];
+    if(sumr ==suml)
+    {
+      flag =1;
+      break;
+    }
+  }
+  if(flag)
+    printf("%d",i);
+  else
+     printf("-1");
+  return 0;
+}
